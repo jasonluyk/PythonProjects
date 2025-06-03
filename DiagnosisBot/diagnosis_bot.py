34f228e918c113962caec42da57cb424f64c32a6
jasonluyk1 <jasonluyk1@gmail.com>
2025-06-03 15:17:17 -0400

    Diagnosis bot

--- DiagnosisBot/diagnosis_bot.py
+++ DiagnosisBot/diagnosis_bot.py
@@ -9,20 +9,44 @@ eye_prompt = "How are the patients eyes?\n - 1: Normal/slightly sunken\n - 2: Ey
 
 skin_prompt = "How is the patient's skin after pinching?\n - 1: Normal skin pinch\n - 2: Slow skin pinch\n"
 
+severe_dehydration = "Severe dehydration indicated"
+some_dehydration = "Some dehydration indicated"
+no_dehydration = "No dehydration indicated"
+
+
+patients_and_diagnoses = []
+
+
+
+
 def patient_list():
-    print("Listing patientsw and diagnoses")
+    print("Listing patients and diagnoses")
+    for patient in patients_and_diagnoses:
+        print(patient)
+
+def save_new_diagnosis(name, diagnosis):
+    if name == "" or diagnosis == "":
+        print("Could not save patient and diagnosis due to invalid input")
+        return
+    final_diagnosis = name + " - " + diagnosis
+    patients_and_diagnoses.append(final_diagnosis)
+    print("Final diagnosis:", final_diagnosis, "\n")
 
 def assess_skin(skin):
     if skin == '1':
-        return "Some dehydration"
+        return some_dehydration
     elif skin == '2':
-        return "Severe dehydration"
+        return severe_dehydration
+    else:
+        return ""
 
 def assess_eyes(eyes):
     if eyes == '1':
-        return "No dehydration indicated"
+        return no_dehydration
     elif eyes == '2':
-        return "Severe dehydration indicated"
+        return severe_dehydration
+    else:
+        return ""
 
 def appearance_check():
     appearance = input(appearance_prompt)
@@ -32,19 +56,21 @@ def appearance_check():
     elif appearance == '2':
         skin = input(skin_prompt)
         assess_skin(skin)
+    else:
+        return ""
 
 
 def start_new_diagnosis():
     name = input(name_prompt)
-    dianosis = assess_appearance()
-    print(name, diagnosis)
+    diagnosis = appearance_check()
+    save_new_diagnosis(name, diagnosis)
 
 
 def main():
     while True:
         selection = input(welcome_prompt)
         if selection == "1":
-            list_patients()
+            patient_list()
         elif selection == "2":
             start_new_diagnosis()
         elif selection == "q":
